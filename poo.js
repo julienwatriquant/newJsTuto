ead88c99915c0f1989b234323f78a23241fbf6f4
julien <julienwatriquant01@gmail.com>
2025-03-18 10:27:30 +0100

    commit de la methode constructor

--- poo.js
+++ poo.js
@@ -13,6 +13,10 @@ but.addEventListener("click" , () =>{
     let o = titre.style.color = "orange";
 })
 
+//--------------------- POO programmation orienté objet-----------------------------
+
+// La methode constructor
+
 function Acteur(prenom,nom,age){
     this.prenom = prenom;
     this.nom = nom;
@@ -22,9 +26,6 @@ function Acteur(prenom,nom,age){
         
     }
 }
-
-
-
 let alain = new Acteur("alain","gebert",56);
 console.log(alain);
 
@@ -47,3 +48,27 @@ let car = new voiture("renault","megane", 2003);
 let car2 = new voiture("bmw","serie 3 m",2021);
 car.presentation();
 car2.presentation();
+
+function pilote(nom,prenom,ecurie){
+    this.nom = nom;
+    this.prenom = prenom;
+    this.ecurie = ecurie;
+}
+
+let driver = new pilote("senna","ayrton",car);
+console.log(driver);
+
+
+function fruit(nom,provenance,prix){
+    this.nom = nom;
+    this.provenance = provenance;
+    this.prix = prix;
+    this.voir = () =>{
+        console.log(this.nom + " venant de "+ this.provenance+" et le prix est de "+this.prix);
+        
+    }
+
+}
+let fruit1 = new fruit("fraise","maroc",5 +" €uros");
+fruit1.voir();
+
